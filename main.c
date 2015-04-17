c2c5d297d5a944edf164001b4b8d3b9c433adf20
Maciej Czekaj <maciej.jakub.czekaj@gmail.com>
2015-04-18 01:45:15 +0200

    indent

--- main.c
+++ main.c
@@ -20,11 +20,12 @@
 #define UART_LCR_DLAB (0x1 << 7)
 
 #define UART_LSR_TEMT (0x1 << 6)
-#define BAUD_115200 13 /*clock 24M / (16*115200) */
+#define BAUD_115200 13		/*clock 24M / (16*115200) */
 #define NO_PARITY      (0)
 #define ONE_STOP_BIT   (0)
 #define DAT_LEN_8_BITS (3)
-#define LC_8_N_1          (NO_PARITY << 3 | ONE_STOP_BIT << 2 | DAT_LEN_8_BITS)
+#define LC_8_N_1  (NO_PARITY << 3 | \
+  ONE_STOP_BIT << 2 | DAT_LEN_8_BITS)
 
 #include <stdint.h>
 
@@ -33,40 +34,48 @@ volatile uint32_t debug = 0xdeadbeef;
 void delay(unsigned cycles)
 {
   unsigned i;
-  for(i = 0; i < cycles; i++ )
+  for (i = 0; i < cycles; i++)
     __asm volatile ("nop"::);
 }
 
-void uart_init(void) {
-  volatile uint32_t *uart_clock = (uint32_t *)0x01C2006C;
-  volatile uint32_t *PB2 = (uint32_t *)0x01C2082C;
-  volatile uint32_t *PB_PULL1 = (uint32_t *)0x01C20844;
-  
+void uart_init(void)
+{
+  volatile uint32_t *uart_clock =
+    (uint32_t *) 0x01C2006C;
+  volatile uint32_t *PB2 =
+    (uint32_t *) 0x01C2082C;
+  volatile uint32_t *PB_PULL1 =
+    (uint32_t *) 0x01C20844;
+
   /* Włączenie zegara dla urządzenia UART na szynie APB */
-  *uart_clock &= ~(1 << (16 + UART_PORT));
+  *uart_clock &=
+    ~(1 << (16 + UART_PORT));
   /* Trzeba odczekać parę cykli zegara */
   delay(100);
-  *uart_clock |=  (1 << (16 + UART_PORT));
+  *uart_clock |=
+    (1 << (16 + UART_PORT));
 
   /* Ustaw piny 22 i 23 portu B 
-    na UART0 RX & UART0_TX */
+     na UART0 RX & UART0_TX */
   *PB2 |= 2 << 28 | 2 << 24;
 
   /* Piny muszą być wyterowane jako pull-up
-    (wartość 2) dla pinów 22 i 23 portu B */
-  *PB_PULL1 |= (2 << ((22-16)*2)) | (2 << ((23-16)*2));
+     (wartość 2) dla pinów 22 i 23 portu B */
+  *PB_PULL1 |=
+    (2 << ((22 - 16) * 2)) | 
+    (2 << ((23 - 16) * 2));
 
   /* Aktywuj konfigurację szybkości transmisji */
   writel(UART_LCR_DLAB, UART_LCR);
 
-  /* Ustaw prędkość portu na 115200 baud*/
+  /* Ustaw prędkość portu na 115200 baud */
   writel(0, UART_DLH);
   writel(BAUD_115200, UART_DLL);
 
   /* Wyłącz konf trasmisji i ustaw:
-       - brak parzystości
-       - 1 bit stopu
-       - długość słowa 8 bitów */
+     - brak parzystości
+     - 1 bit stopu
+     - długość słowa 8 bitów */
   writel(LC_8_N_1, UART_LCR);
 
   delay(100);
@@ -74,21 +83,23 @@ void uart_init(void) {
 
 #define TX_READY (readl(UART_LSR) & UART_LSR_TEMT)
 
-void uart_putc(char c) {
+void uart_putc(char c)
+{
   while (!TX_READY)
     ;
   writel(c, UART_THR);
 }
 
-void uart_puts(const char *s) {
+void uart_puts(const char *s)
+{
   while (*s)
     uart_putc(*s++);
 }
-    
-void main(void) {
+
+void main(void)
+{
   debug = 0x44444444;
   uart_init();
   uart_puts("Hello world!\n\r");
   debug = 0x55555555;
 }
-
